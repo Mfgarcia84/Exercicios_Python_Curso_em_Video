f87d3155d9134bcfb0c1d709cc6272044c559495
Marcelo Figueiredo Garcia <marcelo.garcia1@wipro.com>
2021-11-19 16:19:09 -0300

    Update ex046.py

--- ex046.py
+++ ex046.py
@@ -1,15 +1,25 @@
 #Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
 #indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
 import emoji
-from time import sleep
-print('INICIANDO CONTAGEM REGRESSIVA PARA REVEILLON....')
+import time
 for c in range(10, 0, -1):
-    sleep(1)
     print(c)
-sleep(1)
-print('-='*18)
-print(emoji.emojize(':tada:'*6, use_aliases=True), 'FELIZ 2022!!!', emoji.emojize(':tada:'*6, use_aliases=True))
-print('-='*18)
+    time.sleep(1)
+print('-='*5,'FELIZ ANO NOVO','-='*5)
+print(emoji.emojize(':stuck_out_tongue_closed_eyes:'*20,use_aliases=True))
+
+
+# import emoji
+# from time import sleep
+# print('INICIANDO CONTAGEM REGRESSIVA PARA REVEILLON....')
+# for c in range(10, 0, -1):
+#     sleep(1)
+#     print(c)
+# sleep(1)
+# print('-='*18)
+# print(emoji.emojize(':tada:'*6, use_aliases=True), 'FELIZ 2022!!!', emoji.emojize(':tada:'*6, use_aliases=True))
+# print('-='*18)
 
 
 
