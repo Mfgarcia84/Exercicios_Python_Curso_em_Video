54cf635dc08af850a84d0470f2fa4059e72948ab
Marcelo Figueiredo Garcia <marcelo.garcia1@wipro.com>
2021-11-19 18:15:52 -0300

    update_19112021

--- aulas.py
+++ aulas.py
@@ -1,3 +1,9 @@
 # contador: c = c + 1, sendo c = 0
 # somador: s = s + num, sendo s = 0 e num = algum resultado anterior
 
+#para substituir o for
+c = 1
+while c < 10:#enquanto
+    print(c,end=' ')
+    c = c + 1
+print('\nfim')
---
+++ ex061.py
@@ -0,0 +1,2 @@
+'''Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da PA
+usando o estrutura while'''
