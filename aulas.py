4d91c083cd2c710154b1b37c8a211dd8008f0d30
Marcelo Figueiredo Garcia <marcelo.garcia1@wipro.com>
2021-12-14 22:15:37 -0300

    update_14122021

--- aulas.py
+++ aulas.py
@@ -1,16 +1,7 @@
 # contador: c = c + 1, sendo c = 0
 # somador: s = s + num, sendo s = 0 e num = algum resultado anterior
 
-n = 1
-par = impar = 0
-while n != 0:
-    n = int(input('Digite um número: '))
-    if n!= 0:
-        if n % 2 == 0:
-            par = par + 1
-        else:
-            impar = impar + 1
-print('Você digitou {} número(s) par(es) e {} número(s) ímpar(es).'.format(par, impar))
+
 
 
 
