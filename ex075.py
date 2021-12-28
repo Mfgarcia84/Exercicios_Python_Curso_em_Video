ce7511fc673906fcf3ec06c9f9c760f6df332249
Marcelo Figueiredo Garcia <marcelo.garcia1@wipro.com>
2021-12-28 18:31:31 -0300

    Update_28122021

---
+++ ex075.py
@@ -0,0 +1,35 @@
+'''Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla.
+No final, mostre:
+a) quantas vezes apareceu o valor 9
+b) em que posição foi digitado o primeiro valor 3
+c) quais foram os números pares'''
+
+'''c = 0
+num1 = int(input('Digite o 1º número: '))
+num2 = int(input('Digite o 2º número: '))
+num3 = int(input('Digite o 3º número: '))
+num4 = int(input('Digite o 4º número: '))
+numeros = (num1, num2, num3, num4)
+print(f'O número 9 apareceu {numeros.count(9)} vezes.')
+if numeros.count(3) == 0:
+    print(f'O número 3 não foi digitado nenhuma vez.')
+else:
+    print(f'O número 3 apareceu na {numeros.index(3) + 1}ª posição.')
+for i in range(4):
+    if numeros[i] % 2 == 0:
+        c = c + 1
+print(f'Você digitou {c} números pares.')'''
+    
+#solução da aula
+c = 0
+numeros = (int(input('Digite o 1º número: ')), int(input('Digite o 2º número: ')), #construção da tupla
+        int(input('Digite o 3º número: ')), int(input('Digite o 4º número: ')))
+print(f'O número 9 apareceu {numeros.count(9)} vezes.')
+if 3 not in numeros:
+    print(f'O número 3 não foi digitado nenhuma vez.')
+else:
+    print(f'O número 3 apareceu na {numeros.index(3)+1}ª posição.')
+for i in range(4):
+    if numeros[i] % 2 == 0:
+        c = c + 1
+print(f'Você digitou {c} números pares.')
---
+++ ex076.py
@@ -0,0 +1,16 @@
+'''Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na
+sequência. No final mostre uma listagem de preços, organizando os dados em forma tabular.'''
+
+produtos = ('Mouse', 150, 'Laptop', 7000, 'Monitor', 800, 'Hub', 175, 'Capa Notebook', 90, 'Mochila', 180.5,
+            'Caderno', 30, 'Caneta', 10, 'Borracha', 2.3, 'Lapiseira', 7, 'Grafite', 6.5, 'Lousa', 210)
+print('=='*21)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('=='*21)
+for i in range(len(produtos)):
+    if i % 2 == 0:
+        print(f'{produtos[i]:.<30}', end=' ')
+    else:
+        print(f'R${produtos[i]:>9.2f}')
+print('=='*21)
+   
+    
