f8c92ea0b107ccfc874d5181b79351bf209f7313
Marcelo Figueiredo Garcia <marcelo.garcia1@wipro.com>
2021-12-28 21:14:54 -0300

    Update_28122021

---
+++ ex077.py
@@ -0,0 +1,15 @@
+'''Crie um programa que tenha uma tupla com várias palavras (não usar acentos). 
+Depois disso você deve mostrar, para cada palavra, quais são as suas vogais'''
+
+lista = ('Cerveja', 'Tabaco', 'Cigarro', 'Mesa', 'Escola', 'Python', 'Futebol', 'Pagode', 'Futsamba', 'Marcelo',
+        'Figueiredo', 'Monitor', 'Cadeira', 'Fitbol', 'Carro', 'Velho', 'Nina', 'celular')
+
+
+for palavra in lista:
+    print(f'\nNa palavra {palavra} temos', end=' ')
+    for letra in palavra:
+        if letra in 'AaEeIiOoUu':
+            print(letra, end=' ')
+
+
+   
