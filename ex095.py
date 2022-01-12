1a11535597d153a2379e2d7ee7ea623e6c93faee
Marcelo Figueiredo Garcia <marcelo.garcia1@wipro.com>
2022-01-12 17:21:27 -0300

    Update_12012022

--- ex095.py
+++ ex095.py
@@ -3,11 +3,6 @@ Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um 
 de visualização de detalhes do aproveitamento de cada jogador.
 '''
 
-'''
-Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do
-jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida.
-No final, tudo isso será guardado em um dicionário, incluindo total de gols feitos durante o campeonato.
-'''
 time = []
 jogador = {}
 gols = []
