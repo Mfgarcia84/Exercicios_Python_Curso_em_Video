fa19c271150738d7d1ded2aada702a8dac4b0c6d
Marcelo Figueiredo Garcia <marcelo.garcia1@wipro.com>
2022-01-17 21:44:18 -0300

    Update_17012022

--- ex100.py
+++ ex100.py
@@ -1,3 +1,32 @@
 '''
-
+Faça um programa que tenha uma lista chamada números e duas funções chamadas sorteio() e
+somapar(). A primeira função vai sortear 5 números e vai colocá-la dentro da lista e a
+segunda função vai mostrar a soma entre todos os valores pares sorteados pela função anterior.
 '''
+
+from random import randint
+from time import sleep
+
+
+def sorteio(lista): #sorteia cinco números e insere na lista
+    print('Sorteando 5 valores da lista: ', end='')
+    for jogada in range(5):
+        num = randint(1, 10)
+        lista.append(num)
+        print(f'{num}', end=' ')
+        sleep(0.4)
+    print('PRONTO!')
+
+def somapar(lista): #mostra a soma entre os valores pares da função anterior
+    soma = 0
+    for i in lista:
+        if i % 2 == 0:
+            soma = soma + i
+    print(f'Somando os valores pares de {lista}, temos: {soma}', end='')
+
+
+
+#programa principal
+numeros = []
+sorteio(numeros)
+somapar(numeros)
